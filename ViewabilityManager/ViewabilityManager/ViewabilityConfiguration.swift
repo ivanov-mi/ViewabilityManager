6cc6770cc636a81d3b7b779e26f6835413020423
Martin Ivanov <martin.ivanov@mentormate.com>
2025-03-18 18:31:34 +0200

    Refactor ViewabilityConfiguration

--- ViewabilityManager/ViewabilityManager/ViewabilityConfiguration.swift
+++ ViewabilityManager/ViewabilityManager/ViewabilityConfiguration.swift
@@ -8,84 +8,89 @@
 import UIKit
 
 struct ViewabilityConfiguration {
-    
-    // Default values for configuration parameters
-    enum Defaults {
-        static let areaRatioThreshold = 0.5
-        static let durationThreshold = 1.0
-        static let detectionInterval = 0.1
-        static let alphaThreshold = 0.5
-        static let trackingView: UIView? = nil
-        static let trackingInsets: UIEdgeInsets = .zero
-    }
-    
-    // Private storage for configuration parameters
-    private var _areaRatioThreshold: Double = Defaults.areaRatioThreshold
-    private var _durationThreshold: TimeInterval = Defaults.durationThreshold
-    private var _detectionInterval: Double = Defaults.detectionInterval
-    private var _alphaThreshold: Double = Defaults.alphaThreshold
-    private var _trackingView: UIView? = Defaults.trackingView
-    private var _trackingInsets: UIEdgeInsets = Defaults.trackingInsets
-    
-    // Value from 0 to 1. The view will be impressed if its area ratio remains equal to or greater than this value.
-    var areaRatioThreshold: Double {
-        get { _areaRatioThreshold }
-        set { _areaRatioThreshold = min(max(newValue, 0), 1) }
-    }
-    
-    // Minimum duration time (in seconds) the view must remain visible to be impressed. Value should be positive.
-    var durationThreshold: TimeInterval {
-        get { _durationThreshold }
-        set { _durationThreshold = max(newValue, 0) }
-    }
-    
-    // Detection interval (in seconds). Smaller intervals increase accuracy but consume more resources.
-    var detectionInterval: Double {
-        get { _detectionInterval }
-        set { _detectionInterval = max(newValue, 0) }
-    }
-    
-    // Alpha threshold. Value from 0 to 1. The view will be impressed if its alpha is equal to or greater than this value.
-    var alphaThreshold: Double {
-        get { _alphaThreshold }
-        set { _alphaThreshold = min(max(newValue, 0), 1) }
-    }
-    
-    // Container view for which the tracking is done. If no value is set the tracking will be done for the root view.
-    var trackingView: UIView? {
-        get { _trackingView }
-        set { _trackingView = newValue }
-    }
-    
-    // Insets to reduce the tracked area of the tracking view, e.g. to exclude area hidden behind opaque navigation bars.
-    var trackingInsets: UIEdgeInsets {
-        get { _trackingInsets }
-        set { _trackingInsets = newValue }
-    }
-    
-    // Default configuration values
-    static let `default` = ViewabilityConfiguration(
-        areaRatioThreshold: Defaults.areaRatioThreshold,
-        durationThreshold: Defaults.durationThreshold,
-        detectionInterval: Defaults.detectionInterval,
-        alphaThreshold: Defaults.alphaThreshold,
-        trackingInsets: Defaults.trackingInsets,
-        trackingView: Defaults.trackingView
-    )
-    
-    // Initializer
-    init(areaRatioThreshold: Double = Defaults.areaRatioThreshold,
-         durationThreshold: TimeInterval = Defaults.durationThreshold,
-         detectionInterval: Double = Defaults.detectionInterval,
-         alphaThreshold: Double = Defaults.alphaThreshold,
-         trackingInsets: UIEdgeInsets = Defaults.trackingInsets,
-         trackingView: UIView? = Defaults.trackingView) {
-        self.areaRatioThreshold = areaRatioThreshold
-        self.durationThreshold = durationThreshold
-        self.detectionInterval = detectionInterval
-        self.alphaThreshold = alphaThreshold
-        self.trackingInsets = trackingInsets
-        self.trackingView = trackingView
-    }
+   
+   // Default values for configuration parameters
+   enum Defaults {
+       static let areaRatioThreshold = 0.5
+       static let durationThreshold = 1.0
+       static let detectionInterval = 0.1
+       static let alphaThreshold = 0.5
+       static let trackingView: UIView? = nil
+       static let trackingInsets: UIEdgeInsets = .zero
+   }
+   
+   // Value from 0 to 1. The view will be impressed if its area ratio remains equal to or greater than this value.
+   var areaRatioThreshold: Double {
+       didSet {
+           areaRatioThreshold = ensureZeroToOneRange(areaRatioThreshold)
+       }
+   }
+   
+   // Minimum duration time (in seconds) the view must remain visible to be impressed. Value should be positive.
+   var durationThreshold: TimeInterval {
+       didSet {
+           durationThreshold = ensureNonNegative(durationThreshold)
+       }
+   }
+   
+   // Detection interval (in seconds). Smaller intervals increase accuracy but consume more resources.
+   var detectionInterval: Double {
+       didSet {
+           detectionInterval = ensureNonNegative(detectionInterval)
+       }
+   }
+   
+   // Alpha threshold. Value from 0 to 1. The view will be impressed if its alpha is equal to or greater than this value.
+   var alphaThreshold: Double {
+       didSet {
+           alphaThreshold = ensureZeroToOneRange(alphaThreshold)
+       }
+   }
+   
+   // Container view for which the tracking is done. If no value is set the tracking will be done for the root view.
+   var trackingView: UIView?
+   
+   // Insets to reduce the tracked area of the tracking view, e.g. to exclude area hidden behind opaque navigation bars.
+   var trackingInsets: UIEdgeInsets
+   
+   // Default configuration values
+   static let `default` = ViewabilityConfiguration(
+       trackingInsets: Defaults.trackingInsets,
+       trackingView: Defaults.trackingView,
+       areaRatioThreshold: Defaults.areaRatioThreshold,
+       durationThreshold: Defaults.durationThreshold,
+       detectionInterval: Defaults.detectionInterval,
+       alphaThreshold: Defaults.alphaThreshold
+   )
+   
+   // Initializer with validation
+   init(trackingInsets: UIEdgeInsets = Defaults.trackingInsets,
+        trackingView: UIView? = Defaults.trackingView,
+        areaRatioThreshold: Double = Defaults.areaRatioThreshold,
+        durationThreshold: TimeInterval = Defaults.durationThreshold,
+        detectionInterval: Double = Defaults.detectionInterval,
+        alphaThreshold: Double = Defaults.alphaThreshold) {
+       
+       // Initialise all stored properties
+       self.trackingInsets = trackingInsets
+       self.trackingView = trackingView
+       self.areaRatioThreshold = areaRatioThreshold
+       self.durationThreshold = durationThreshold
+       self.detectionInterval = detectionInterval
+       self.alphaThreshold = alphaThreshold
+       
+       // Validate properties
+       self.areaRatioThreshold = ensureZeroToOneRange(self.areaRatioThreshold)
+       self.durationThreshold = ensureNonNegative(self.durationThreshold)
+       self.detectionInterval = ensureNonNegative(self.detectionInterval)
+       self.alphaThreshold = ensureZeroToOneRange(self.alphaThreshold)
+   }
+   
+   private func ensureZeroToOneRange(_ value: Double) -> Double {
+       return min(max(value, 0), 1)
+   }
+   
+   private func ensureNonNegative(_ value: Double) -> Double {
+       return max(value, 0)
+   }
 }
-
